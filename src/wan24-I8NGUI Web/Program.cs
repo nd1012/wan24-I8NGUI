b0ec9d2226606fb51b83130d34ab7f1e22e19bf6
nd <nd1012@users.noreply.github.com>
2024-03-17 03:05:20 +0100

    Update Program.cs

--- src/wan24-I8NGUI Web/Program.cs
+++ src/wan24-I8NGUI Web/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.FluentUI.AspNetCore.Components;
 using wan24.Core;
 using wan24.I8NGUI;
 
-await Bootstrap.Async().DynamicContext();
+await Bootstrap.Async();
 Translation.Current = Translation.Dummy;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -14,4 +14,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
 
-await builder.Build().RunAsync().DynamicContext();
+await builder.Build().RunAsync();
